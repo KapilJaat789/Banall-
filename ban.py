7845e4faf092331ec0224628f62257a84254e367
KapilJaat789 <163076128+KapilJaat789@users.noreply.github.com>
2024-05-10 08:36:57 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -63,7 +63,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"**𝚃𝚑𝚎 𝚂𝚙𝚎𝚎𝚍 𝚘𝚏 𝙲𝚎𝚗𝚣𝚘 ⚡** \n\n 𝙼𝚢 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 @FAKEXENZO! `{ms}` ms")
+        await event.edit(f"**𝚃𝚑𝚎 𝚂𝚙𝚎𝚎𝚍 𝚘𝚏 Total Network⚡** \n\n 𝙼𝚢 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 @ITACHI_TNs! `{ms}` ms")
 
 
 @Riz.on(events.NewMessage(pattern="^/kickall"))
@@ -80,7 +80,7 @@ async def kickall(event):
          creator = RiZ.creator
          if not admin and not creator:
               return await event.reply("𝙸 𝚍𝚘𝚗'𝚝 𝙷𝚊𝚟𝚎 𝚜𝚞𝚏𝚏𝚒𝚌𝚒𝚎𝚗𝚝 𝚁𝚒𝚐𝚑𝚝𝚜!!")
-         RiZoeL = await Riz.send_message(event.chat_id, "**𝚃𝚑𝚎 𝚂𝚙𝚎𝚎𝚍 𝚘𝚏 𝙲𝚎𝚗𝚣𝚘 ⚡**")
+         RiZoeL = await Riz.send_message(event.chat_id, "**𝚃𝚑𝚎 𝚂𝚙𝚎𝚎𝚍 𝚘𝚏 Total Network ⚡**")
          admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
          admins_id = [i.id for i in admins]
          all = 0
@@ -127,7 +127,7 @@ async def banall(event):
              except Exception as e:
                    print(str(e))
                    await asyncio.sleep(0.1)
-         await RiZoeL.edit(f"**𝚄𝚜𝚎𝚛𝚜 𝙱𝚊𝚗𝚗𝚎𝚍 𝚂𝚞𝚌𝚌𝚎𝚜𝚜𝚏𝚞𝚕𝚕𝚢, 𝚃𝚑𝚎 𝙵𝚎𝚠 𝚃𝚑𝚎 𝙵𝚎𝚊𝚛𝚕𝚎𝚜𝚜⚡⚡! \n\n 𝙱𝚊𝚗𝚗𝚎𝚍 𝚄𝚜𝚎𝚛𝚜:** `{bann}` \n **𝚃𝚘𝚝𝚊𝚕 𝚄𝚜𝚎𝚛𝚜:** `{all}`")
+         await RiZoeL.edit(f"**Group fucked successful by TN , 𝚃𝚑𝚎 𝙵𝚎𝚠 𝚃𝚑𝚎 𝙵𝚎𝚊𝚛𝚕𝚎𝚜𝚜⚡⚡! \n\n 𝙱𝚊𝚗𝚗𝚎𝚍 𝚄𝚜𝚎𝚛𝚜:** `{bann}` \n **𝚃𝚘𝚝𝚊𝚕 𝚄𝚜𝚎𝚛𝚜:** `{all}`")
 
     
 @Riz.on(events.NewMessage(pattern="^/unbanall"))
